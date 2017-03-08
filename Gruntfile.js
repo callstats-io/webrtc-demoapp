2048900ab9f49c1d7e2f8c9dee6fc4ccdc0b04d2
Lennart Schulte <lennart@callstats.io>
2017-03-08 13:12:15 +0200

    use eslint with Google style guide

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,24 +1,18 @@
 module.exports = function(grunt) {
-
   grunt.initConfig({
-    jshint: {
-      files: ['Gruntfile.js', 'app/*.js'],
+    eslint: {
       options: {
-        globals: {
-          jQuery: true
-        },
-        esversion: 6
+        configFile: '.eslintrc',
       },
+      target: ['app/*.js']
     },
     watch: {
-      files: ['<%= jshint.files %>'],
-      tasks: ['jshint']
+      files: ['<%= eslint.target %>'],
+      tasks: ['eslint']
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
-
-  grunt.registerTask('default', ['jshint']);
-
+  grunt.loadNpmTasks('grunt-eslint');
+  grunt.registerTask('default', ['eslint']);
 };
