1dfb2b448233a2e47f5cd2bdfd51c3ab35b7731a
pallab-gain <pallab@callstats.io>
2018-03-27 15:46:04 +0600

    Fix for some class to className, and css attributes.

---
+++ src/client/components/meetingpage/AppLayout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export class AppLayout extends React.Component {
+  render() {
+    return (
+      <div className={'container-fluid'}>
+        <h2>Meeting page</h2>
+      </div>
+    );
+  }
+}
+
+export default AppLayout;
--- src/client/router.js
+++ src/client/router.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
 
-import AppLayout from 'components/applayout/AppLayout';
+import AppLayout from 'components/landingpage/AppLayout';
 
 export default function() {
   return (
