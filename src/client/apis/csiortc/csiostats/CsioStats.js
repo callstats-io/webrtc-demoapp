2fbb0d5986108aa46429e8b687e28981ee25e336
pallab-gain <pallab@callstats.io>
2018-04-05 00:04:24 +0600

    Send peer connection related events.

--- src/client/apis/csiortc/csiostats/CsioStats.js
+++ src/client/apis/csiortc/csiostats/CsioStats.js
@@ -7,9 +7,19 @@ class CsioStats {
   constructor() {
     this.isInitialized = false;
     this.config = {};
+    this.roomName = undefined;
     this.csObject = new callstats();
     this.csObject.on('defaultConfig', this.defaultConfigCallback.bind(this));
     this.csObject.on('recommendedConfig', this.recommendedConfigCallback.bind(this));
+    document.addEventListener(
+      CsioEvents.MEETING_PAGE.ON_MEETING_PAGE_LOADED,
+      this.onMeetingPageLoaded.bind(this), false);
+    document.addEventListener(
+      CsioEvents.CsioPeerConnection.ON_WEBRTC_ERROR,
+      this.handleWebrtcError.bind(this), false);
+  }
+  onMeetingPageLoaded(e) {
+    this.roomName = e.detail.roomName;
   }
   initialize(userID) {
     if (this.isInitialized) {
@@ -46,6 +56,38 @@ class CsioStats {
   csStatsCallback(stats) {
     console.log('stats callback');
   };
+  // handle webrtc errors
+  handleWebrtcError(e) {
+    const roomName = this.roomName;
+    const pcObject = e.detail.pc;
+    const err = e.detail.error;
+    const type = e.detail.type;
+    let csioType;
+    switch (type) {
+      case 'createOffer':
+        csioType = this.csObject.webRTCFunctions.createOffer;
+        break;
+      case 'createAnswer':
+        csioType = this.csObject.webRTCFunctions.createAnswer;
+        break;
+      case 'setLocalDescription':
+        csioType = this.csObject.webRTCFunctions.setLocalDescription;
+        break;
+      case 'setRemoteDescription':
+        csioType = this.csObject.webRTCFunctions.setRemoteDescription;
+        break;
+      case 'addIceCandidate':
+        csioType = this.csObject.webRTCFunctions.addIceCandidate;
+        break;
+      case 'getUserMedia':
+        csioType = this.csObject.webRTCFunctions.getUserMedia;
+        break;
+      default:
+        console.log('Error', type, 'not handled!');
+        return;
+    }
+    this.csObject.reportError(pcObject, roomName, csioType, err);
+  }
 }
 
 export default CsioStats;
