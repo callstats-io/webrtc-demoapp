22fcb599d0d2b68b05486981908d4d2751ed1c39
pallab-gain <pallab@callstats.io>
2018-04-06 17:41:40 +0600

    Adding getUser media test.

---
+++ __tests__/DemoApp.js
@@ -0,0 +1,8 @@
+'use strict';
+describe('LandingPage', function() {
+  describe('#getUserMedia', function() {
+    describe('#audioOnly', function() {});
+    describe('#videoOnly', function() {});
+    describe('#both', function() {});
+  });
+});
