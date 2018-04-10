6c4457ef7517facb82a783858225a716bdd0973e
pallab-gain <pallab@callstats.io>
2018-04-10 17:44:33 +0600

    Showing modal popup for precall test Result

--- src/client/components/handlers/meetingpage/PrecalltestResultPopupHandler.js
+++ src/client/components/handlers/meetingpage/PrecalltestResultPopupHandler.js
@@ -6,14 +6,22 @@ class PrecalltestResultPopupHandler {
   }
   getState() {
     return {
-      showModal: this.showModal
+      showModal: this.showModal,
+      mediaConnectivity: false,
+      rtt: 0,
+      fractionalLoss: 0,
+      throughput: 0
     };
   }
   showPrecallStats(e) {
-    this.precallStats = e.detail.precallStats;
-    console.log('->', this.precallStats);
+    const precallStats = e.detail.precallStats;
+    console.warn('->', precallStats);
     this.setState({
-      showModal: 'block'
+      showModal: 'block',
+      mediaConnectivity: (precallStats.connectivity) ? 'True' : 'False',
+      rtt: (precallStats.rtt || 0).toFixed(2),
+      fractionalLoss: (precallStats.fractionalLoss || 0).toFixed(2),
+      throughput: (precallStats.throughput || 0).toFixed(2)
     });
   }
   handleCloseModal(e) {
--- src/client/components/views/meetingpage/popup/PrecalltestResultPopup.js
+++ src/client/components/views/meetingpage/popup/PrecalltestResultPopup.js
@@ -22,18 +22,48 @@ class PrecalltestResultPopup extends React.Component {
         <div className="modal-dialog" role="document">
           <div className="modal-content">
             <div className="modal-header">
-              <h4 className="modal-title"> <strong>Precall test result : </strong></h4>
+              <h4 className="modal-title"> <strong>Precall test result: </strong></h4>
             </div>
             <div className="modal-body">
               <div className={'row'}>
-                <div className={'col-xs-3'}/>
-                <div className={'col-xs-3'}>
-                  <p> <strong> Overall:  </strong></p>
+                <div className={'col-xs-2'}/>
+                <div className={'col-xs-4'}>
+                  <p> <strong> Connectivity :  </strong></p>
                 </div>
-                <div className={'col-xs-3'}>
-                  <p> <strong> Overall:  </strong></p>
+                <div className={'col-xs-4'}>
+                  <p> <strong> {this.state.mediaConnectivity}  </strong></p>
                 </div>
-                <div className={'col-xs-3'}/>
+                <div className={'col-xs-2'}/>
+              </div>
+              <div className={'row'}>
+                <div className={'col-xs-2'}/>
+                <div className={'col-xs-4'}>
+                  <p> <strong> RTT :  </strong></p>
+                </div>
+                <div className={'col-xs-4'}>
+                  <p> <strong> {this.state.rtt} ms  </strong></p>
+                </div>
+                <div className={'col-xs-2'}/>
+              </div>
+              <div className={'row'}>
+                <div className={'col-xs-2'}/>
+                <div className={'col-xs-4'}>
+                  <p> <strong> Fractional Loss :  </strong></p>
+                </div>
+                <div className={'col-xs-4'}>
+                  <p> <strong> {this.state.fractionalLoss}  </strong></p>
+                </div>
+                <div className={'col-xs-2'}/>
+              </div>
+              <div className={'row'}>
+                <div className={'col-xs-2'}/>
+                <div className={'col-xs-4'}>
+                  <p> <strong> Throughput :  </strong></p>
+                </div>
+                <div className={'col-xs-4'}>
+                  <p> <strong> {this.state.throughput} kbps  </strong></p>
+                </div>
+                <div className={'col-xs-2'}/>
               </div>
               <div className="modal-footer">
                 <button type="button" className="btn btn-primary"
