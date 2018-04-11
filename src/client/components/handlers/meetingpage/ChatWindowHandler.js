8e50a9bf983fecb62a5924cbbd026dd6c9f70c17
pallab-gain <pallab@callstats.io>
2018-04-11 14:01:20 +0600

    Fix chat layout sizing issue. Now can take full screen size.

--- src/client/components/handlers/meetingpage/ChatWindowHandler.js
+++ src/client/components/handlers/meetingpage/ChatWindowHandler.js
@@ -9,11 +9,22 @@ class ChatWindowHandler {
     this.chatMessages = [];
   }
 
+  maybeUpdateResizeWindow() {
+    setTimeout(function() {
+      const fixedHeight = this.state.windowHeight +
+        (window.innerHeight - document.getElementById('principle-dom').offsetHeight);
+      // console.warn('doing a resize ', this.state.windowHeight, fixedHeight);
+      this.setState({
+        windowHeight: fixedHeight
+      });
+    }.bind(this), 1 * 1000);
+  }
   getState() {
     return {
       inputText: this.inputText,
       chatMessages: this.chatMessages,
-      chatText: this.chatText
+      chatText: this.chatText,
+      windowHeight: 360
     };
   }
   onInputChange(e) {
--- src/client/components/views/meetingpage/AppLayout.js
+++ src/client/components/views/meetingpage/AppLayout.js
@@ -22,7 +22,7 @@ export class AppLayout extends React.Component {
   }
   render() {
     return (
-      <div className={'container-fluid'}>
+      <div className={'container-fluid'} id='principle-dom'>
         <HeaderLayout roomName={this.roomName}/>
         <AlertLayout/>
         <ContentLayout/>
